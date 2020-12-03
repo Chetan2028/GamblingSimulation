bf4f4348872b2b220b35de668a10ed54b4cc474f
Chetan2028 <bmchetan2028@gmail.com>
2020-12-03 10:31:13 +0530

    [Chetan] Add : Added bet method

--- GamblingSimulation/GamblingMain.cs
+++ GamblingSimulation/GamblingMain.cs
@@ -5,12 +5,27 @@ namespace GamblingSimulation
     class GamblingMain
     {
         //Variables
-        const int STAKE = 100;
-        const int BET = 1;
+        public const int STAKE = 100;
+        public const int BET = 1;
 
+        public void Bet()
+        {
+            Random random = new Random();
+            int choice = random.Next(0, 1);
+            if (choice == 1)
+            {
+                Console.WriteLine("Gambler won 1$ ");
+            }
+            else
+            {
+                Console.WriteLine("Gambler lost 1$");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulation Problem");
+            GamblingMain gambling = new GamblingMain();
+            gambling.Bet();
         }
     }
 }
