a7432d8f3471bbb97dc844d52581f3c2bc648f2d
Chetan2028 <bmchetan2028@gmail.com>
2020-12-03 09:58:09 +0530

    [Chetan] Add : Added Bet and stake for gambling game

--- GamblingSimulation/Main.cs
+++ GamblingSimulation/Main.cs
@@ -2,12 +2,13 @@
 
 namespace GamblingSimulation
 {
-    class Main
+    class GamblingMain
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulation Problem");
-
+            int stake = 100;
+            int bet = 1;
         }
     }
 }
