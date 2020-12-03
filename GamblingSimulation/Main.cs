6d34b63375cb0759cfa958e3123a5fed764c9c7b
Chetan2028 <bmchetan2028@gmail.com>
2020-12-03 09:55:13 +0530

    Add project files.

---
+++ GamblingSimulation/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GamblingSimulation
+{
+    class Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling Simulation Problem");
+
+        }
+    }
+}
