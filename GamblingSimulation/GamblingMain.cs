c013e3226a165c5b9b89d0a7bdbe6eb66e792878
Chetan2028 <bmchetan2028@gmail.com>
2020-12-03 11:36:46 +0530

    [Chetan] Add : Added to gamble until reaches 50% of stake either win or loose

--- GamblingSimulation/GamblingMain.cs
+++ GamblingSimulation/GamblingMain.cs
@@ -7,25 +7,45 @@ namespace GamblingSimulation
         //Variables
         public const int STAKE = 100;
         public const int BET = 1;
+        public static int updatedStake = STAKE;
 
-        public void Bet()
+        public static void Bet()
         {
             Random random = new Random();
-            int choice = random.Next(0, 1);
-            if (choice == 1)
+            int randomChoice = random.Next(2);
+            if (randomChoice == 1)
             {
+                updatedStake += BET;
                 Console.WriteLine("Gambler won 1$ ");
             }
             else
             {
+                updatedStake -= BET;
                 Console.WriteLine("Gambler lost 1$");
             }
         }
+
+        public static void CalculativeGambler()
+        {
+            int i = 2000;
+            while (i > 0)
+            {
+                Bet();
+                Console.WriteLine(updatedStake);
+                if (updatedStake == STAKE / 2 || (updatedStake == STAKE + STAKE / 2))
+                {
+                    break;
+                }
+                i--;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulation Problem");
             GamblingMain gambling = new GamblingMain();
-            gambling.Bet();
+            Bet();
+            CalculativeGambler();
         }
     }
 }
